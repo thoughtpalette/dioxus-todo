ee9a2962d14ea893aa37b0e08eabffe4988dff7a
Chris Marshall <thoughtpalettechris@gmail.com>
2022-12-28 21:41:57 -0600

    kind of can now add items to the list using the use state apis

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use dioxus::prelude::*;
+use dioxus::{prelude::*, events::{oninput, onkeydown}};
 
 fn main() {
     dioxus::web::launch(app);
 }
 
+
 fn app(cx: Scope) -> Element {
-    let mut list_items = vec!["Todo1", "Todo2", "Todo3"];
+    let initial_todo = vec!["This is the first Todo"];
+    let todos = use_state(&cx, || initial_todo);
+    let draft = use_state(&cx, || "".to_string());
+    let mut count = use_state(&cx, || 1);
 
     rsx!(cx, main {
             style { 
@@ -17,15 +21,31 @@ fn app(cx: Scope) -> Element {
                 h1 { "Todays Todos" }
             }
             ul { class: "todo-list",
-
-                {list_items.iter().map(|item| rsx! {
+                {todos.iter().map(|item| rsx! {
                     li {
                         key: "{item}",
                         div { "{item}" }
                     }
                 })} 
             }
-            footer { class: "footer"}
+            input {
+                placeholder: "What are you doing today?",
+                value: "{draft}",
+                oninput: move |evt| draft.set(evt.value.clone()),
+                onkeydown: move |evt| {
+                    if evt.key == "Enter" && !draft.is_empty() {
+                        todos.make_mut().insert(*count.current(), &draft);
+                        draft.set("".to_string());
+                        count += 1;
+                    }
+                }
+            }
+            footer { class: "footer", 
+                button {
+                    onclick: move |_| todos.with_mut(|v| v.push("yeet")),
+                    "Add"
+                }
+            }
         }
     )
 }
