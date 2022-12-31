759bf8d61e54ca31104f75183fcd1b3b7971935a
Chris Marshall <thoughtpalettechris@gmail.com>
2022-12-31 12:53:40 -0600

    Sunsetting this, framework is not API ready for me to build

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dioxus-todo
-Using the Dioxus framework for a TODO list app.
+Using the Dioxus framework for a TODO list app
+
+## ON HOLD
+Documenation for the web portion is nowhere near completed enough for me to build as well as the examples are using v0.1.8 and this version is 0.2.4. The APIs are too different. Going to go with something else...
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use dioxus::{prelude::*, events::{oninput, onkeydown}};
+use dioxus::prelude::*;
 
 fn main() {
     dioxus::web::launch(app);
 }
 
+#[derive(Debug, PartialEq, Clone)]
+struct Todo {
+    pub key: u64,
+    pub todo: String,
+}
 
 fn app(cx: Scope) -> Element {
-    let initial_todo = vec!["This is the first Todo"];
-    let todos = use_state(&cx, || initial_todo);
-    let draft = use_state(&cx, || "".to_string());
-    let mut count = use_state(&cx, || 1);
+    // let todos = use_state(&cx, || im_rc::HashMap::<u32, Todo>::default);
+    let (todos, set_todos) = use_state(&cx, || vec![]);
+    let input_value = use_state(&cx, || String::from(""));
+    let mut _count = use_state(&cx, || 1);
+
+    // .map(|item| 0rsx! {
+    //     li {
+    //         key: "{item}",
+    //         div { "{item}" }
+    //     }
+    // })} 
 
     rsx!(cx, main {
             style { 
@@ -21,30 +33,32 @@ fn app(cx: Scope) -> Element {
                 h1 { "Todays Todos" }
             }
             ul { class: "todo-list",
-                {todos.iter().map(|item| rsx! {
+                {todos.clone().iter().map(|item| rsx! {
                     li {
                         key: "{item}",
                         div { "{item}" }
                     }
-                })} 
+                }
+                )}
             }
             input {
                 placeholder: "What are you doing today?",
-                value: "{draft}",
-                oninput: move |evt| draft.set(evt.value.clone()),
+                value: "{input_value}",
+                oninput: move |evt| input_value.set(evt.value.clone()),
                 onkeydown: move |evt| {
-                    if evt.key == "Enter" && !draft.is_empty() {
-                        todos.make_mut().insert(*count.current(), &draft);
-                        draft.set("".to_string());
-                        count += 1;
+                    if evt.key == "Enter" && !input_value.is_empty() {
+                        // todos.make_mut().insert(*count.current(), &input_value);
+                        // input_value.set("".to_string());
+                        println!("{}", input_value.get());
+                        // count += 1;
                     }
                 }
             }
             footer { class: "footer", 
-                button {
-                    onclick: move |_| todos.with_mut(|v| v.push("yeet")),
-                    "Add"
-                }
+                // button {
+                //     onclick: move |_| todos.write().push(&input_value),
+                //     "Add"
+                // }
             }
         }
     )
